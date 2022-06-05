0027be677c3977ab4e9936002870b732129375ce
AndrewJBateman <gomezbateman@yahoo.com>
2022-06-05 19:04:52 +0200

    refactor: user position shown on map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,14 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
 import tt from '@tomtom-international/web-sdk-maps';
+import { Observable, Subscriber } from 'rxjs';
+
+import { environment } from '../environments/environment';
+import { Position } from './interfaces/position.model';
 
 @Component({
   selector: 'app-root',
@@ -7,15 +16,65 @@ import tt from '@tomtom-international/web-sdk-maps';
   styleUrls: ['./app.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements AfterViewInit {
   title = 'angular-tomtom-api';
-  API_KEY = 'QsoDyjAiFZJxo731Gt4tJnB7F24KcdBx';
+  API_KEY = environment.tomtom.key;
+  map: any;
+  //initialise position with coords of the Eiffel Tower
+  position: Position = { latitude: 48.858093, longitude: 2.294694 };
 
-  ngOnInit(): any {
-    const map = tt.map({
+  ngOnInit(): any {}
+
+  ngAfterViewInit(): void {
+    this.mapInit();
+  }
+
+  private getUserPosition = (): Observable<Position> => {
+    const userPosition = new Observable((observer: Subscriber<any>) => {
+      navigator.geolocation
+        ? navigator.geolocation.getCurrentPosition((pos: any) => {
+            observer.next({
+              latitude: pos.coords.latitude,
+              longitude: pos.coords.longitude,
+            });
+            observer.complete();
+          })
+        : observer.error();
+    });
+    return userPosition;
+  };
+
+  private mapInit(): void {
+    this.map = tt.map({
       key: this.API_KEY,
       container: 'map',
     });
-    map.addControl(new tt.NavigationControl());
+
+    this.map.addControl(new tt.NavigationControl());
+    this.map.addControl(new tt.FullscreenControl());
+
+    this.getUserPosition().subscribe((pos: Position) => {
+      this.map.flyTo({
+        center: {
+          lat: pos.latitude,
+          lng: pos.longitude,
+        },
+        zoom: 14,
+      });
+
+      const popup = new tt.Popup({
+        anchor: 'bottom',
+        offset: { bottom: [0, -40] },
+      }).setHTML(
+        `Your position:<br/><b>latitude:</b> ${pos.latitude}, <br/><b>longitude:</b> ${pos.longitude}`
+      );
+      const marker = new tt.Marker()
+        .setLngLat({
+          lat: pos.latitude,
+          lng: pos.longitude,
+        })
+        .addTo(this.map);
+      marker.setPopup(popup).togglePopup();
+    });
   }
 }
